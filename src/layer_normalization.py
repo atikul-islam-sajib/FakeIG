a58f8478eb46f1eb0328b98dd7b2d130587f285e
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2025-08-17 14:27:22 +0200

    layer norm is almost completed

--- src/layer_normalization.py
+++ src/layer_normalization.py
@@ -6,21 +6,48 @@ import torch.nn as nn
 
 sys.path.append("./src")
 
+
 class LayerNormalization(nn.Module):
     def __init__(self, dimension: int = 512, eps: float = 1e-5):
         super(LayerNormalization, self).__init__()
-        
+
         self.dimension = dimension
         self.eps = eps
-        
-        self.alpha = nn.Parameter(torch.ones((self.dimension//self.dimension, self.dimension//self.dimension, self.dimension)))
-        self.gamma = nn.Parameter(torch.zeros((self.dimension//self.dimension, self.dimension//self.dimension, self.dimension)))
-        
+
+        self.gamma = nn.Parameter(
+            torch.ones(
+                (
+                    self.dimension // self.dimension,
+                    self.dimension // self.dimension,
+                    self.dimension,
+                )
+            )
+        )
+        self.betas = nn.Parameter(
+            torch.zeros(
+                (
+                    self.dimension // self.dimension,
+                    self.dimension // self.dimension,
+                    self.dimension,
+                )
+            )
+        )
+
     def forward(self, x: torch.Tensor):
         if not isinstance(x, torch.Tensor):
             raise TypeError("Input must be a torch.Tensor".capitalize())
-        
+
+        mean = torch.mean(input=x, dim=-1, keepdim=True)
+        variance = torch.var(input=x, dim=-1, keepdim=True)
+
+        x = (x - mean) / torch.sqrt(variance + self.eps)
+
+        return self.gamma * x + self.betas
+
+
 if __name__ == "__main__":
     normalization = LayerNormalization(dimension=512, eps=1e-5)
-    
+
     images = torch.randn((16, 64, 512))
+
+    print(normalization(images).size())
