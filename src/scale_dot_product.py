d24ad13447c1eae4e1e9e520e32533b8e0c666d6
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2025-08-17 14:09:02 +0200

    scale dot product is completed

--- src/scale_dot_product.py
+++ src/scale_dot_product.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import math
 import torch
 import torch.nn as nn
 
@@ -12,8 +13,27 @@ def scale_dot_product(query: torch.Tensor, key: torch.Tensor, value: torch.Tenso
         or not isinstance(key, torch.Tensor)
         or not isinstance(value, torch.Tensor)
     ):
-        raise TypeError("All inputs must be torch.Tensor")
+        raise TypeError("Images must be a torch.Tensor".capitalize())
+
+    else:
+        weights = torch.matmul(
+            input=query, other=torch.transpose(input=key, dim0=-2, dim1=-1)
+        )
+        weights = weights / math.sqrt(query.size(-1))
+
+        attention = torch.softmax(input=weights, dim=-1)
+        attention = torch.matmul(input=attention, other=value)
+
+        return attention
 
 
 if __name__ == "__main__":
-    pass
+    query = torch.randn((16, 8, 64, 64))
+    key = torch.randn((16, 8, 64, 64))
+    value = torch.randn((16, 8, 64, 64))
+
+    attention = scale_dot_product(query=query, key=key, value=value)
+
+    assert(attention.size()) == query.size(), "Attention must be the same size as query".capitalize()
+    assert (attention.size()) == key.size(), "Attention must be the same size as key".capitalize()
+    assert (attention.size()) == value.size(), "Attention must be the same size as value".capitalize()
