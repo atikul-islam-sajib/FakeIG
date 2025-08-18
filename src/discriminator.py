b37a4b2e3638a71ae58e436dac4ef57828961c81
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2025-08-18 17:59:07 +0200

    netD is almost completed

--- src/discriminator.py
+++ src/discriminator.py
@@ -3,18 +3,21 @@ import sys
 import torch
 import argparse
 import torch.nn as nn
+from tqdm import tqdm
 
 sys.path.append("./src")
 
 try:
     from patch_embedding import PatchEmbedding
+    from transformer_encoder_block import TransformerEncoderBlock
 except ImportError:
     print("Import cannot be found".capitalize())
-    
+
 
 class Discriminator(nn.Module):
     def __init__(
         self,
+        image_channels: int = 3,
         image_size: int = 224,
         patch_size: int = 8,
         num_layers: int = 4,
@@ -26,6 +29,7 @@ class Discriminator(nn.Module):
         layer_norm_eps: float = 1e-5,
     ):
         super(Discriminator, self).__init__()
+        self.image_channels = image_channels
         self.image_size = image_size
         self.patch_size = patch_size
         self.num_layers = num_layers
@@ -35,21 +39,57 @@ class Discriminator(nn.Module):
         self.dropout = dropout
         self.activation = activation
         self.layer_norm_eps = layer_norm_eps
-        
+
+        self.patch_embedding = PatchEmbedding(
+            image_size=self.image_size,
+            patch_size=self.patch_size,
+            image_channels=self.image_channels,
+            dimension=self.d_model,
+        )
+
+        self.transformer_blocks = nn.Sequential(
+            *[
+                TransformerEncoderBlock(
+                    d_model=self.d_model,
+                    nhead=self.nhead,
+                    dim_feedforward=self.dim_feedforward,
+                    dropout=self.dropout,
+                    activation=self.activation,
+                    layer_norm_eps=self.layer_norm_eps,
+                )
+                for _ in tqdm(range(self.num_layers))
+            ]
+        )
+
+        self.classifier = nn.Linear(
+            in_features=self.d_model, out_features=self.d_model // self.d_model
+        )
+
     def forward(self, x: torch.Tensor):
         if not isinstance(x, torch.Tensor):
             raise TypeError("Input must be a torch.Tensor".capitalize())
-        
-        
+
+        x = self.patch_embedding(x)
+        for transformer in self.transformer_blocks:
+            x = transformer(x)
+
+        x = torch.mean(input=x, dim=1)
+        x = self.classifier(x).view(-1)
+
+        return x
+
+
 if __name__ == "__main__":
     netD = Discriminator(
         image_size=224,
         patch_size=8,
-        num_layers=4,
+        num_layers=2,
         d_model=512,
         nhead=8,
         dim_feedforward=2048,
-        dropout=0.1,    
+        dropout=0.1,
         activation="gelu",
-        layer_norm_eps=1e-5
+        layer_norm_eps=1e-5,
     )
+    images = torch.randn((4, 3, 224, 224))
+    print(netD(images).shape)
