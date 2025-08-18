4327e22b50bf7c94d54bfe2cd359e2b3703f98bb
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2025-08-18 17:41:46 +0200

    transformer encoder block is completed almost

--- src/transformer_encoder_block.py
+++ src/transformer_encoder_block.py
@@ -6,6 +6,13 @@ import torch.nn as nn
 
 sys.path.append("./src")
 
+try:
+    from layer_normalization import LayerNormalization
+    from multihead_attention_layer import MultiHeadAttentionLayer
+    from feedforward_neural_network import FeedForwardNeuralNetwork
+except ImportError:
+    print("Import cannot be found".capitalize())
+
 
 class TransformerEncoderBlock(nn.Module):
     def __init__(
@@ -24,11 +31,49 @@ class TransformerEncoderBlock(nn.Module):
         self.dropout = dropout
         self.activation = activation
         self.layer_norm_eps = layer_norm_eps
+
+        self.normalization1 = LayerNormalization(
+            dimension=self.d_model, eps=self.layer_norm_eps
+        )
+        self.normalization2 = LayerNormalization(
+            dimension=self.d_model, eps=self.layer_norm_eps
+        )
         
+        self.dropout1 = nn.Dropout(p=self.dropout)
+        self.dropout2 = nn.Dropout(p=self.dropout)
+        
+        self.multihead_attention = MultiHeadAttentionLayer(
+            heads=self.nheads,
+            dimension=self.d_model,
+            eps=self.layer_norm_eps,
+        )
+        self.neural_network = FeedForwardNeuralNetwork(
+            dimension=self.d_model,
+            dropout=self.dropout,
+            activation=self.activation,
+        )
+
     def forward(self, x: torch.Tensor):
         if not isinstance(x, torch.Tensor):
             raise TypeError("Input must be a torch.Tensor".capitalize())
         
+        residual = x
+        
+        x = self.multihead_attention(x)
+        x = self.dropout1(x)
+        x = torch.add(input=x, other=residual)
+        x = self.normalization1(x)
+        
+        residual = x
+        
+        x = self.neural_network(x)
+        x = self.dropout2(x)
+        x = torch.add(input=x, other=residual)
+        x = self.normalization2(x)
+        
+        return x
+
+
 if __name__ == "__main__":
     transformer = TransformerEncoderBlock(
         d_model=512,
@@ -36,5 +81,7 @@ if __name__ == "__main__":
         dim_feedforward=2048,
         dropout=0.1,
         activation="gelu",
-        layer_norm_eps=1e-5
+        layer_norm_eps=1e-5,
     )
+    images = torch.randn((16, 768, 512))
+    print(transformer(images).size())
