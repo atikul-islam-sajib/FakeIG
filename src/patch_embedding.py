4923623841d0c8cf79d7d3193a22d9db46f3537e
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2025-08-18 11:52:12 +0200

    patch embedding is almost completed

--- src/patch_embedding.py
+++ src/patch_embedding.py
@@ -6,18 +6,65 @@ import torch.nn as nn
 
 sys.path.append("./src")
 
+
 class PatchEmbedding(nn.Module):
-    def __init__(self, image_size: int = 224, patch_size: int = 8):
+    def __init__(
+        self,
+        image_size: int = 224,
+        patch_size: int = 8,
+        image_channels: int = 3,
+        dimension: int = 512,
+    ):
         super(PatchEmbedding, self).__init__()
         self.image_size = image_size
         self.patch_size = patch_size
-        
+        self.image_channels = image_channels
+        self.dimension = dimension
+
+        assert (
+            self.image_size % self.patch_size == 0
+        ), "Image size must be divisible by patch size".title()
+
+        self.num_of_patches = (self.image_size // self.patch_size) ** 2
+
+        self.kernel_size = patch_size
+        self.stride_size = patch_size
+        self.padding_size = int(self.patch_size // self.patch_size)
+
+        if self.dimension == 0:
+            self.dimension = (self.padding_size**2) * self.image_channels
+
+        self.projection = nn.Conv2d(
+            in_channels=self.image_channels,
+            out_channels=self.dimension,
+            kernel_size=self.kernel_size,
+            stride=self.stride_size,
+            padding=self.padding_size,
+        )
+
+        self.positional_encoding = nn.Parameter(
+            torch.randn(
+                self.patch_size // self.patch_size,
+                self.patch_size // self.patch_size,
+                self.dimension,
+            )
+        )
+
     def forward(self, x: torch.Tensor):
         if not isinstance(x, torch.Tensor):
             raise TypeError("Input should be in torch format".capitalize())
-        
+
+        x = self.projection(x)
+        x = x.view(x.size(0), x.size(1), x.size(2) * x.size(3))
+        x = x.permute(0, 2, 1)
+        x = x + self.positional_encoding
+
+        return x
+
+
 if __name__ == "__main__":
     embedding = PatchEmbedding(
-        image_size=224,
-        patch_size=8
+        image_size=224, patch_size=8, image_channels=3, dimension=512
     )
+    images = torch.randn((16, 3, 224, 224))
+    print(embedding(images).size())
