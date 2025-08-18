826f20e4b3432a891f3924a3c65a4a75718f2289
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2025-08-18 17:44:11 +0200

    transformer encoder block is completed

--- src/transformer_encoder_block.py
+++ src/transformer_encoder_block.py
@@ -38,10 +38,10 @@ class TransformerEncoderBlock(nn.Module):
         self.normalization2 = LayerNormalization(
             dimension=self.d_model, eps=self.layer_norm_eps
         )
-        
+
         self.dropout1 = nn.Dropout(p=self.dropout)
         self.dropout2 = nn.Dropout(p=self.dropout)
-        
+
         self.multihead_attention = MultiHeadAttentionLayer(
             heads=self.nheads,
             dimension=self.d_model,
@@ -56,32 +56,67 @@ class TransformerEncoderBlock(nn.Module):
     def forward(self, x: torch.Tensor):
         if not isinstance(x, torch.Tensor):
             raise TypeError("Input must be a torch.Tensor".capitalize())
-        
+
         residual = x
-        
+
         x = self.multihead_attention(x)
         x = self.dropout1(x)
         x = torch.add(input=x, other=residual)
         x = self.normalization1(x)
-        
+
         residual = x
-        
+
         x = self.neural_network(x)
         x = self.dropout2(x)
         x = torch.add(input=x, other=residual)
         x = self.normalization2(x)
-        
+
         return x
 
 
 if __name__ == "__main__":
-    transformer = TransformerEncoderBlock(
-        d_model=512,
-        nhead=8,
-        dim_feedforward=2048,
-        dropout=0.1,
-        activation="gelu",
-        layer_norm_eps=1e-5,
+    parser = argparse.ArgumentParser(description="Transformer Encoder Block".title())
+    parser.add_argument(
+        "--d_model", type=int, default=512, help="Dimension".capitalize()
     )
-    images = torch.randn((16, 768, 512))
-    print(transformer(images).size())
+    parser.add_argument(
+        "--nhead", type=int, default=8, help="Number of heads".capitalize()
+    )
+    parser.add_argument(
+        "--dim_feedforward", type=int, default=2048, help="Dimension".capitalize()
+    )
+    parser.add_argument(
+        "--dropout", type=float, default=0.1, help="Dropout".capitalize()
+    )
+    parser.add_argument(
+        "--activation",
+        type=str,
+        default="gelu",
+        help="Activation function".capitalize(),
+    )
+    parser.add_argument(
+        "--layer_norm_eps", type=float, default=1e-5, help="Epsilon".capitalize()
+    )
+
+    args = parser.parse_args()
+
+    d_model = args.d_model
+    nhead = args.nhead
+    dim_feedforward = args.dim_feedforward
+    dropout = args.dropout
+    activation = args.activation
+    layer_norm_eps = args.layer_norm_eps
+
+    transformer = TransformerEncoderBlock(
+        d_model=d_model,
+        nhead=nhead,
+        dim_feedforward=dim_feedforward,
+        dropout=dropout,
+        activation=activation,
+        layer_norm_eps=layer_norm_eps,
+    )
+
+    images = torch.randn((16, 768, d_model))
+    assert (
+        transformer(images).size()
+    ) == images.size(), "Transformer must be the same size as images".capitalize()
