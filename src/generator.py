7ac9dbac92d8b3fd56ee17f59c670898f79073fb
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2025-08-19 12:17:29 +0200

    netG is almost completed

--- src/generator.py
+++ src/generator.py
@@ -3,8 +3,14 @@ import sys
 import torch
 import argparse
 import torch.nn as nn
+from tqdm import tqdm
 
 sys.path.append("./src")
+
+try:
+    from transformer_encoder_block import TransformerEncoderBlock
+except ImportError:
+    print("Import cannot be found".capitalize())
 
 
 class Decoder(nn.Module):
@@ -15,6 +21,7 @@ class Decoder(nn.Module):
 class Generator(nn.Module):
     def __init__(
         self,
+        latent_size: int = 100,
         image_channels: int = 3,
         image_size: int = 224,
         patch_size: int = 8,
@@ -27,7 +34,8 @@ class Generator(nn.Module):
         layer_norm_eps: float = 1e-5,
     ):
         super(Generator, self).__init__()
-        
+
+        self.latent_size = latent_size
         self.image_channels = image_channels
         self.image_size = image_size
         self.patch_size = patch_size
@@ -38,10 +46,51 @@ class Generator(nn.Module):
         self.dropout = dropout
         self.activation = activation
         self.layer_norm_eps = layer_norm_eps
-        
+
+        self.num_of_patches = (self.image_size // self.patch_size) ** 2
+
+        self.projection = nn.Linear(
+            in_features=self.latent_size,
+            out_features=self.num_of_patches * self.d_model,
+        )
+
+        self.positional_encoding = nn.Parameter(
+            torch.ones((1, 1, self.d_model)), requires_grad=True
+        )
+
+        self.transformer_blocks = nn.Sequential(
+            *[
+                TransformerEncoderBlock(
+                    d_model=self.d_model,
+                    nhead=self.nhead,
+                    dim_feedforward=self.dim_feedforward,
+                    dropout=self.dropout,
+                    activation=self.activation,
+                    layer_norm_eps=self.layer_norm_eps,
+                )
+                for _ in tqdm(
+                    range(self.num_layers),
+                    desc=f"Initializing Transformer with {self.num_layers} layers".capitalize(),
+                )
+            ]
+        )
+
     def forward(self, x: torch.Tensor):
         if not isinstance(x, torch.Tensor):
             raise TypeError("Input must be a torch.Tensor".capitalize())
+
+        x = x.squeeze()
+        x = self.projection(x)
+        x = x.view(x.size(0), self.num_of_patches, self.d_model)
+
+        x = x + self.positional_encoding
+
+        for transformer in self.transformer_blocks:
+            x = transformer(x)
+
+        x = x.permute(0, 2, 1)
+        return x
+
 
 if __name__ == "__main__":
     netG = Generator(
@@ -56,3 +105,7 @@ if __name__ == "__main__":
         activation="gelu",
         layer_norm_eps=1e-5,
     )
+
+    images = torch.randn((16, 100, 1, 1))
+
+    print(netG(images).size())
